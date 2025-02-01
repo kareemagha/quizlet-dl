acaebfbfe9c2a7279ab36ac0ee8339e9c32f8058
KAREEM AGHA <79729084+kareemagha@users.noreply.github.com>
2025-02-01 18:31:53 +1100

    feat: multiple solutions can now be copied over

--- src/scripts/answerScraper.ts
+++ src/scripts/answerScraper.ts
@@ -1,42 +1,42 @@
 function getAnswer() {
-    const element1 = document.getElementsByClassName("s1i7awl8")
-    console.log(element1.length)
-
-    for (let i = 0; i < element1.length; i++) {
-        const currentElement = element1[i];
-    }
-
-    const element = document.querySelector('#mainContainer > main > div > div > div > div > main > div > div > div > div:nth-child(2) > div > div.s1i7awl8');
+    const element = document.getElementsByClassName("s1i7awl8")
+    console.log(element.length)
     if (element) {
-        const styles = window.getComputedStyle(element);
-        let styleString = '';
-        for (let i = 0; i < styles.length; i++) {
-            const property = styles[i];
-            styleString += `${property}: ${styles.getPropertyValue(property)}; `;
-        }
-        element.setAttribute('style', styleString);
-        const HTMLAnswer = element.outerHTML;
-        const pageURL = window.location.toString();
-        console.log(HTMLAnswer);
-        console.log(pageURL)
+        for (let i = 0; i < element.length; i++) {
+            const currentElement = element[i];
+            const styles = window.getComputedStyle(currentElement);
+            let styleString = '';
+            for (let i = 0; i < styles.length; i++) {
+                const property = styles[i];
+                styleString += `${property}: ${styles.getPropertyValue(property)}; `;
+            }
+            currentElement.setAttribute('style', styleString);
+            const HTMLAnswer = currentElement.outerHTML;
+            const pageURL = window.location.toString();
+            console.log(HTMLAnswer);
+            console.log(pageURL)
 
-        let pageTitle
+            let pageTitle
 
-        const URLparts = pageURL.split('/');
-        console.log(URLparts)
-        if (URLparts.length >= 7) {
-            console.log(URLparts[6])
-            let pageName = URLparts[6].split('-').slice(0, -5)
-            pageName = pageName.map(word => {
-                return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-            });
-            pageTitle = pageName.join(' ')
-        }
-        
-        var port = chrome.runtime.connect({name: "answers"});
-        port.postMessage({
-            html: HTMLAnswer,
-            name: pageTitle});
+            const URLparts = pageURL.split('/');
+            console.log(URLparts)
+            if (URLparts.length >= 7) {
+                console.log(URLparts[6])
+                let pageName = URLparts[6].split('-').slice(0, -5)
+                pageName = pageName.map(word => {
+                    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+                });
+                pageTitle = pageName.join(' ')
+                if(element.length > 1) {
+                    pageTitle = pageTitle + ` (${i + 1})`
+                }
+            }
+            
+            var port = chrome.runtime.connect({name: "answers"});
+            port.postMessage({
+                html: HTMLAnswer,
+                name: pageTitle});
+        }    
     } else {
         console.warn("Answer element not found")
     }
