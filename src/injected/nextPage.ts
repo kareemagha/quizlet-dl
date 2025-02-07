3b4b18ffcaad8cbfbe4db9cd6f92602026f22e99
KAREEM AGHA <79729084+kareemagha@users.noreply.github.com>
2025-02-07 14:01:24 +1100

    chore: updated nextPage.ts to use props data

--- src/injected/nextPage.ts
+++ src/injected/nextPage.ts
@@ -1,17 +1,11 @@
 (function() {
 
-    // const button1Selector = '#mainContainer > main > div > div > div > div > main > div > div > div > div:nth-child(3) > div > div.b1opuclq > div > div.n5cc71p > div > a';
-    // const button2Selector = '#mainContainer > main > div > div > div > div > main > div > div > div > div:nth-child(2) > div > div.b1opuclq > div > div.n5cc71p > div > a';
-    // console.log("flipper injected")
+    const scriptElement = document.getElementById('__NEXT_DATA__');
+    const jsonData = scriptElement?.textContent ? JSON.parse(scriptElement.textContent) : null;
 
-    // const element = document.querySelectorAll(".s1i7awl8")
-    // const elementLength = element.length;
+    if (jsonData) {
+        const nextPage = jsonData.props.pageProps.exercise.nextExercises[0]._webUrl;
+        window.location.href = nextPage;
+    }
 
-    // const button1 = document.querySelector(button1Selector) as HTMLElement | null;
-    // const button2 = document.querySelector(button2Selector) as HTMLElement | null;
-    // if (elementLength > 1 && button1) {
-    //     button1.click();
-    // } else if (button2) {
-    //     button2.click();
-    // }
 })()
