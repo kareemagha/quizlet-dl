2b2db831c8b1e5aa042667fb2a48139068cad17f
KAREEM AGHA <79729084+kareemagha@users.noreply.github.com>
2025-02-08 23:19:22 +1100

    chore: updatved version number

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { ManifestV3 } from 'rollup-plugin-chrome-extension'
 const manifest: ManifestV3 = {
   name: "quizlet-dl",
   description: "download quizlet textbook solutions as a pdf",
-  version: "1.1.0",
+  version: "2.0.0",
   manifest_version: 3,
   permissions: ["activeTab", "tabs", "scripting", "storage"],
   host_permissions: ["https://quizlet.com/explanations/textbook-solutions/*"],
